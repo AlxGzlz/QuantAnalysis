d81fa0dfe55341350c821afbcb04976ced59a718
Alex <88829257+AlxGzlz@users.noreply.github.com>
2023-01-11 22:31:21 +0100

    Update risk_ratio_analysis.py
    Change in the method to analyze the Sharpe & Sortino ratios over the time period

--- risk_ratio_analysis.py
+++ risk_ratio_analysis.py
@@ -1,6 +1,6 @@
 ratio_analysis = pd.DataFrame()
 #calculate the rolling Sharpe ratio over the loaded time period
-ratio_analysis['rolling_sharpe'] = qt.stats.rolling_sharpe(df_returns)
+ratio_analysis['rolling_sharpe'] = qt.stats.rolling_sharpe(df_returns, rolling_period=126)
 #calculate the Sortino ratio over the loaded time period 
 ratio_analysis['rolling_sortino'] = qt.stats.rolling_sortino(df_returns)
 
@@ -9,33 +9,50 @@ ratio_analysis['rolling_sortino'] = qt.stats.rolling_sortino(df_returns)
 #the risk to get/keep the asset is considerdd too high to get a higher return than a risk-free asset
 ratio_analysis['sharpe above sortino'] = pta.above(ratio_analysis['rolling_sharpe'], ratio_analysis['rolling_sortino'])
 
-#compare when Sharpe ratio is below 0
+#identify when Sharpe ratio is below 0
 ratio_analysis['sharpe below 0'] = pta.below_value(ratio_analysis['rolling_sharpe'], 0)
-#compare when Sortino ratio is below 0
+#identify when Sortino ratio is below 0
 ratio_analysis['sortino below 0'] = pta.below_value(ratio_analysis['rolling_sortino'], 0)
 
-x = 0
-y = x+1
-#identify when the Sortino ratio goes above 0 => bullish signal
-while ratio_analysis['sortino below 0'][x]==ratio_analysis['sortino below 0'][y]:
-    x+=1
-    if ratio_analysis['sortino below 0'][x]>ratio_analysis['sortino below 0'][y]:
-    sortino_bullish_signal[x] = ratio_analysis.index[x]
+#count the total number of different values in the select dataset
+def count_rows(df, column_name):
+    value_counts = df[column_name].value_counts()
+    return value_counts.to_dict()
 
-#identify when the Sortino ratio goes below 0 => bearish signal
-while ratio_analysis['sortino below 0'][x]==ratio_analysis['sortino below 0'][y]:
-	x+=1
-	if ratio_analysis['sortino below 0'][x]<ratio_analysis['sortino below 0'][y]:
-	sortino_bearish_signal[x] = ratio_analysis.index[x]
-	
-#identify when the Sharpe ration goes above 0 => bullish signal
-while ratio_analysis['sharpe below 0'][x]==ratio_analysis['sharpe below 0'][y]:
-    x+=1
-    if ratio_analysis['sharpe below 0'][x]>ratio_analysis['sharpe below 0'][y]:
-    sharpe_bullish_signal[x] = ratio_analysis.index[x]
+#apply the above function for the historical results of the Sortino ratio above 0
+sortino_rows = count_rows(ratio_analysis, 'sortino below 0')
+#apply the above function for the historical results of the Sharpe ratio above 0
+sharpe_rows = count_rows(ratio_analysis, 'sharpe below 0')
+#apply the above function for the historical results of the Sharpe ratio above the Sortino ratio
+risk_profit_rows = count_rows(ratio_analysis, 'sharpe above sortino')
 
-#identify when the Sharpe ration goes below 0 => bearish signal
-while ratio_analysis['sharpe below 0'][x]==ratio_analysis['sharpe below 0'][y]:
-    x+=1
-    if ratio_analysis['sharpe below 0'][x]<ratio_analysis['sharpe below 0'][y]:
-    sharpe_bearish_signal[x] = ratio_analysis.index[x]]
+#identify the % of days when the Sharpe ratio is under or above 0 during the selected time period
+av_negative_sortino = ((days - sortino_rows[1]) - 1)/days*100
+av_positive_sortino = ((days - sortino_rows[0]) - 1)/days*100
+#identify the % of days when the Sharpe ratio is under or above 0 during the selected time period
+av_negative_sharpe = ((days - sharpe_rows[1]) - 1)/days*100
+av_positive_sharpe = ((days - sharpe_rows[0]) - 1)/days*100
+#identify the % of days when the Sharpe ratio is under or above the Sortino ratio during the selected time period
+av_negative_risk = ((days - risk_profit_rows[1]) - 1)/days*100
+av_positive_risk = ((days - risk_profit_rows[0]) - 1)/days*100
+
+if av_negative_sortino < av_positive_sortino:
+    comment_av_sortino = 1
+else: comment_av_sortino = 0,5
+    
+if av_negative_sharpe < av_positive_sharpe:
+    comment_av_sharpe = 1
+else: comment_av_sharpe = 0,5
+    
+if av_negative_risk < av_positive_risk:
+    comment_av_risk = 2
+else: comment_av_risk = 1
+    
+result_av = (float(comment_av_sortino) + float(comment_av_sharpe) + float(comment_av_risk)
+
+if result_av >=3.5:
+    return "risk mitigated over the period"
+elif result_av == 3:
+    return "significant risk over the period"
+elif result_av >= 2:
+    return "high risk/low return over the period"
