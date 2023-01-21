6c3d5e0aa71f8ec8808b6ba627109527823920ca
Alex <88829257+AlxGzlz@users.noreply.github.com>
2023-01-21 19:04:16 +0100

    Update price_trend_analysis.py
    Add the Kalman ratio to the price analysis

--- price_trend_analysis.py
+++ price_trend_analysis.py
@@ -1,22 +1,39 @@
 #calculate the average price of the loaded time period
-average_price = float(np.mean(df_day['close']))
+average_price = float(np.mean(df_day['Close']))
 average_price = round(average_price, 2)
 #integrate the average price in the dataframe of the loaded instrument
 df_day['average price'] = average_price
-#calculate the average return of the loaded time period
-average_return = float(average_return)*100
-average_return = round(average_return, 4)
 #calculate the Simple Moving Average (sma)
-sma = pta.sma(df_day["close"])
+sma = pta.sma(df_day["Close"])
 #calculate the Exponential Moving Average
-ema = pta.ema(df_day["close"])
+ema = pta.ema(df_day["Close"])
+
+#based on the PQN025 Newsletter (https://pyquantnews.com/kalman-filter-openbb-for-parameterless-indicators/)
+#computation of the Kalman Ratio for the closing prices of the asset
+#define the parameter to compute the Kalman ratio
+kf = KalmanFilter(
+    transition_matrices = [1],
+    observation_matrices = [1],
+    initial_state_mean = 0,
+    initial_state_covariance = 1,
+    observation_covariance=1,
+    transition_covariance=0.01)
+
+#compute the Kalman ratio for the asset price*
+def compute_Kalman(a):
+    state_means, _ = kf.filter(a)
+    state_means = pd.Series(state_means.flatten(), index=a.index)
+    return state_means
+
+Kalman_df = compute_Kalman(df_day['Close'])
 
 %matplotlib widget
 plt.title("Daily prices evolution of "+ ticker[0])
-plt.plot(df_day['close'], label="price", c="blue")
+plt.plot(df_day['Close'], label="price", c="blue")
 plt.plot(df_day['average price'], label="avg price", c="red")
-plt.plot(sma, label ="SMA", c="green")
-plt.plot(ema, label = "EMA", c="yellow")
+#plt.plot(sma, label ="SMA", c="green")
+#plt.plot(ema, label = "EMA", c="yellow")
+plt.plot(Kalman_df, label='Kalman', c="orange")
 plt.xlabel("datetime")
 plt.ylabel("price in $")
 plt.legend()
