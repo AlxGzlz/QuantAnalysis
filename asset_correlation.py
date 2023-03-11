197b79dbde849d47958678ffa96ae99a12ab7754
Alex <88829257+AlxGzlz@users.noreply.github.com>
2023-03-11 14:11:34 +0100

    Update and rename hedge_beta.py to asset_correlation.py
    update correlation calculation

--- asset_correlation.py
+++ asset_correlation.py
@@ -3,8 +3,9 @@
 #hedge the beta of your selected ticker or portfolio
 X = bench_returns.values
 Y = df_returns.values
+Z = riskfree_returns.values
 #define the linear regression for the selected ticker
-def linreg(x,y): 
+def linreg(x,y,z): 
     #add a column of ls to fit alpha
     x = sm.add_constant(x)
     model = regression.linear_model.OLS(y,x).fit()
@@ -12,29 +13,30 @@ def linreg(x,y):
     x = x[:, 1]
     return model.params[0], model.params[1]
 
-alpha, beta = linreg(X,Y)
+alpha, beta = linreg(X,Y,Z)
 
 #seek to hedge the beta of the selected ticker, meaning to get beta = 0
 #be short a number of shares in the market equal to the beta plus ticker
 hedged_portfolio = -1 * beta * bench_returns + df_returns
 hedged_portfolio.name = "Heged Portfolio"
 P = hedged_portfolio.values 
-alpha, hedged_beta = linreg(X, P)
+alpha, hedged_beta = linreg(X,P,Z)
 
-def linreg_pred(df,ind):
-    pred = df*ind
-    return pred
+#calculate the correlation in the price evolution between the selected asset and benchmark
+def corrcoef(df,bc):
+    a=np.corrcoef(df,bc)[0,1]
+    a=round(a,4)
+    return a
 
-beta_pred=linreg_pred(df_day['Close'],beta)
-hedgedbeta_pred=linreg_pred(df_day['Close'],hedged_beta)
+correlation_coef=corrcoef(df_day['Close'],bench_day['Close'])
 
-#plot the results
-%matplotlib widget
-plt.title('Comparison between '+t+' price and its linear regression prediction')
-plt.plot(df_day['Close'])
-plt.plot(beta_pred)
-plt.ylabel('Price in $')
-plt.legend(['Asset','Linear Regression Prediction'])
 print(f"Alpha:",alpha)
 print(f"Beta:",beta)
 print(f"Hedged Beta:",hedged_beta)
+print(f"Correlation coefficient between "+t+" and "+b+" :",correlation_coef)
+
+%matplotlib widget
+plt.title("Prices correlation between"+t+" and "+b)
+plt.ylabel(b)
+plt.xlabel(t)
+plt.scatter(df_day['Close'],bench_day['Close'])
